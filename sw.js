4e2a36530f3d0c326fa5508a3091b78ba9bcc3f6
huhan-h <72545060@qq.com>
2026-03-02 19:27:59 +0800

    Site updated: 2026-03-02 19:27:59

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1772450354492";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1772450874322";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
